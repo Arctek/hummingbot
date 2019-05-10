459952df4e60a4a7e4b8a38253b8dad051ee2111
Joshua Richardson <joshua@webcoders.com.au>
2019-05-10 15:57:33 +0930

    Continued

--- hummingbot/download_token_addresses.py
+++ hummingbot/download_token_addresses.py
@@ -12,6 +12,7 @@ from web3 import Web3
 
 DDEX_ENDPOINT = "https://api.ddex.io/v3/markets"
 RADAR_RELAY_ENDPOINT = "https://api.radarrelay.com/v2/markets?perPage=100&page=1"
+BAMBOO_RELAY_ENDPOINT = "https://rest.bamboorelay.com/main/0x/markets?perPage=500&page=1"
 API_CALL_TIMEOUT = 5
 
 
@@ -25,6 +26,23 @@ async def download_ddex_token_addresses(token_dict: Dict[str, str]):
                     for market in markets:
                         base = market.get("baseToken")
                         quote = market.get("quoteToken")
+                        if base not in token_dict:
+                            token_dict[base] = Web3.toChecksumAddress(market.get("baseTokenAddress"))
+                        if quote not in token_dict:
+                            token_dict[quote] = Web3.toChecksumAddress(market.get("quoteTokenAddress"))
+                except Exception as err:
+                    logging.getLogger().error(err)
+
+
+async def download_bamboo_relay_token_addresses(token_dict: Dict[str, str]):
+    async with aiohttp.ClientSession() as client:
+        async with client.get(RADAR_RELAY_ENDPOINT, timeout=API_CALL_TIMEOUT) as response:
+            if response.status == 200:
+                try:
+                    markets = await response.json()
+                    for market in markets:
+                        market_id = market.get("id")
+                        base, quote = market_id.split("-")
                         if base not in token_dict:
                             token_dict[base] = Web3.toChecksumAddress(market.get("baseTokenAddress"))
                         if quote not in token_dict:
@@ -52,6 +70,7 @@ async def download_radar_relay_token_addresses(token_dict: Dict[str, str]):
 
 async def download_erc20_token_addresses(token_dict: Dict[str, str] = {}):
     await download_radar_relay_token_addresses(token_dict)
+    await download_bamboo_relay_token_addresses(token_dict)
     await download_ddex_token_addresses(token_dict)
 
 
